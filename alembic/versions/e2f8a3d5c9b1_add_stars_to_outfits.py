c898c0f7184787c22cbcbdca58b911adc2d86a3a
yaroslav pisarev <yaroslav@yaroslavs-MacBook-Pro.local>
2026-06-07 11:52:01 +0300

    mig

--- alembic/versions/e2f8a3d5c9b1_add_stars_to_outfits.py
+++ alembic/versions/e2f8a3d5c9b1_add_stars_to_outfits.py
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = 'e2f8a3d5c9b1'
-down_revision: Union[str, None] = 'b7d4f1e2a903'
+down_revision: Union[str, None] = '99267fcc6b62'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
